db721fa1ae7dbe3945a0f453d768e16fcbd4616e
Sahil Choudhary <>
2022-09-17 09:07:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function problem()
+  {
+    document.getElementById("problem").innerHTML="Build three tables with information as shown in below screenshot using only HTML tags";
+    document.getElementById("fname").innerHTML="Sahil Choudhary";
+    document.getElementById("contact").innerHTML="9622257671";
+    document.getElementById("course").innerHTML="FULL STACK DEVELOPMENT";
+    document.getElementById("email").innerHTML="sahilchoudhary4566@gmail.com";
+    document.getElementById("component").innerHTML=" using the elements, table, th, td property colspan and row span) and hr tag In CSS:font-weight,border,font-family";
+    document.getElementById("Explain").innerHTML="The Table elemnt created is used for storin the data, the headings are the 'information headings' and the data is the 'details' . the border and each row has been given border. in hoobies the table is provided with an minimum width and height to table appear as its. In student scores table the row and colspan are ued, with the corresponding <td> and <tr> tags.";
+  }
+
+function problem2()
+  {
+    document.getElementById("problem").innerHTML="Build a various elements shown below using tables and other HTML tags without using CSS";
+    document.getElementById("fname").innerHTML="Sahil Choudhary";
+    document.getElementById("contact").innerHTML="9622257671";
+    document.getElementById("course").innerHTML="FULL STACK DEVELOPMENT";
+    document.getElementById("email").innerHTML="sahilchoudhary4566@gmail.com";
+     document.getElementById("component").innerHTML="Using the elements, table, th, td, ul, ol, dl, properties: dt,dd, li, the list style property is used, as respectively for , circle, disc,square, alpha, roman, number, with an property of lower-roman,lower-aplha, and start.";
+   document.getElementById("Explain").innerHTML="The Table elemnt created is used for storing the data, the headings are the information headings and the data is the details . The border of the table is set to 1, which making each border visible for rows and table, Each list is made under a row in table with the heading specifying the nature of list. start is used to initialise list at our choice."}
+
+
+function problem3()
+  {
+    document.getElementById("problem").innerHTML="Build a product listing page as shown in below screenshot";
+    document.getElementById("fname").innerHTML="Sahil Choudhary";
+    document.getElementById("contact").innerHTML="9622257671";
+    document.getElementById("course").innerHTML="FULL STACK DEVELOPMENT";
+    document.getElementById("email").innerHTML="sahilchoudhary4566@gmail.com";
+    document.getElementById("component").innerHTML="The components used are Basic HTML and CSS, table, Div, button, radio, select. fonts and the icons as from google and font awesome. buttons are also used ";
+    document.getElementById("Explain").innerHTML="Multiple Div are used in template div, seperate div is there for item and details which is in tray div using flex attripute to display. details are enterd using nested tables, the CSS has been used, to color, align, class and id selectors are mainly used. ";
+  }
